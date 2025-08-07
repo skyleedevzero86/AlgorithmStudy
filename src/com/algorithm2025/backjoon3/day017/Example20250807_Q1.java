6b0d9d9e34838cb078c90c5f92f6062069580781
이경용 <sleekydz86@icloud.com>
2025-08-07 23:56:34 +0900

    feat: 코딩테스트 연습 연습문제 최적의 행렬 곱셈

---
+++ src/com/algorithm2025/backjoon3/day017/Example20250807_Q1.java
@@ -0,0 +1,33 @@
+package com.algorithm2025.backjoon3.day017;
+
+public class Example20250807_Q1 { // 코딩테스트 연습 연습문제 최적의 행렬 곱셈
+
+    public  int Example20250807_Q1 (int[][] matrixSizes) {
+        int n = matrixSizes.length;
+        int[][] dp = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                dp[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = 0;
+        }
+
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+                for (int k = i; k < j; k++) {
+                    dp[i][j] = Math.min(dp[i][j],
+                            dp[i][k] + dp[k + 1][j] + matrixSizes[i][0] * matrixSizes[k + 1][0] * matrixSizes[j][1]);
+                }
+            }
+        }
+
+        return dp[0][n - 1];
+    }
+
+
+}
