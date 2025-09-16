2d65e5b7df67c7133964d455b24ed71378871681
이경용 <sleekydz86@icloud.com>
2025-09-16 23:10:46 +0900

    코딩테스트 연습  코딩 기초 트레이닝 덧셈식 출력하기

---
+++ src/com/algorithm2025/backjoon4/day006/Example20250916_Q181947.java
@@ -0,0 +1,15 @@
+package com.algorithm2025.backjoon4.day006;
+
+import java.util.Scanner;
+
+public class Example20250916_Q181947 { //코딩테스트 연습  코딩 기초 트레이닝 덧셈식 출력하기
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        sc.close();
+        int result=a+b;
+
+        System.out.println(a +" + "+ b +" = "+result);
+    }
+}
