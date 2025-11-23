7dc689f81ca6bcf4a6c579bc8a9829767053fdd8
이경용 <sleekydz86@icloud.com>
2025-11-23 19:55:50 +0900

    feat: 지형이동 복습

---
+++ src/com/algorithm2025/backjoon4/day022/Example20251123_Q62050.java
@@ -0,0 +1,51 @@
+package com.algorithm2025.backjoon4.day022;
+
+import java.util.PriorityQueue;
+
+public class Example20251123_Q62050 { // 코딩테스트 연습 Summer/Winter Coding(2019) 지형 이동
+
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+
+    public int solution(int[][] land, int height) {
+        int n = land.length;
+        boolean[][] visited = new boolean[n][n];
+        PriorityQueue<int[]> heap = new PriorityQueue<>((a, b) -> a[0] - b[0]);
+        heap.offer(new int[]{0, 0, 0});
+        int answer = 0;
+
+        while (!heap.isEmpty()) {
+            int[] cur = heap.poll();
+            int v = cur[0];
+            int x = cur[1];
+            int y = cur[2];
+
+            if (visited[x][y]) {
+                continue;
+            }
+
+            visited[x][y] = true;
+            answer += v;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || nx >= n || ny < 0 || ny >= n || visited[nx][ny]) {
+                    continue;
+                }
+
+                int diff = Math.abs(land[x][y] - land[nx][ny]);
+
+                if (diff > height) {
+                    heap.offer(new int[]{diff, nx, ny});
+                } else {
+                    heap.offer(new int[]{0, nx, ny});
+                }
+            }
+        }
+
+        return answer;
+    }
+
+}
