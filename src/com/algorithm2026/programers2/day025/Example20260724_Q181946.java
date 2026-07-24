bfed536e7f0c25e82826a637b8a8c288c903fba9
이경용 <sleekydz86@icloud.com>
2026-07-24 22:46:25 +0900

    feat: 문자열 붙여서출력하기

---
+++ src/com/algorithm2026/programers2/day025/Example20260724_Q181946.java
@@ -0,0 +1,12 @@
+package com.algorithm2026.programers2.day025;
+
+public class Example20260724_Q181946 { //문자열 붙여서 출력하기
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        sc.close();
+        String result=a+b;
+        System.out.println(result);
+    }
+}
